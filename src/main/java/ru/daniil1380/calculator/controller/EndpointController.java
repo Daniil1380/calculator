998d273d8cc07fdad632c9971a3c8a1566ac95c1
d.tkachenko <d.tkachenko@argustelecom.ru>
2021-11-24 19:11:59 +0300

    closes #15, логика вынесена в отдельный сервис для более удобного тестирования

--- src/main/java/ru/daniil1380/calculator/controller/EndpointController.java
+++ src/main/java/ru/daniil1380/calculator/controller/EndpointController.java
@@ -1,41 +1,24 @@
 package ru.daniil1380.calculator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.daniil1380.calculator.CalculatorService;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import java.util.List;
 
 @RestController
 public class EndpointController {
 
+    @Autowired
+    CalculatorService calculatorService;
+
     @GetMapping(value = "api/calculator")
     public ResponseEntity<Double> getTestData(@RequestParam(name = "exp") String expression) throws ScriptException {
-        expression = addSpecificSymbols(expression);
-        expression = expression + ";";
-        expression = expression.replace(" ", "+");
-        System.out.println(expression);
-        ScriptEngineManager factory = new ScriptEngineManager();
-        ScriptEngine engine = factory.getEngineByName("JavaScript");
-        if (engine.eval(expression) instanceof Double) {
-            return ResponseEntity.ok((Double) engine.eval(expression));
-        }
-        if (engine.eval(expression) instanceof Integer) {
-            return ResponseEntity.ok(((Integer) engine.eval(expression)).doubleValue());
-        }
-        return ResponseEntity.ok(0.0);
+        return ResponseEntity.ok(calculatorService.calculate(expression));
     }
 
-    private String addSpecificSymbols(String expression){
-        expression = expression.replace("e", "Math.E");
-        expression = expression.replace("pi", "Math.PI");
-        expression = expression.replace("Pi", "Math.PI");
-        expression = expression.replace("pI", "Math.PI");
-        return expression;
-    }
 
 }
